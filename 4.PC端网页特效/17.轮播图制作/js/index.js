8368ef513643112de9d4f9b16ffdc6ee7237805f
Vacricticy <987540432@qq.com>
2020-06-18 23:13:45 +0800

    study

--- 4.PC端网页特效/17.轮播图制作/js/index.js
+++ 4.PC端网页特效/17.轮播图制作/js/index.js
@@ -1,129 +1,137 @@
-window.addEventListener('load', function() {
-    // 1. 获取元素
-    var arrow_l = document.querySelector('.arrow-l');
-    var arrow_r = document.querySelector('.arrow-r');
-    var focus = document.querySelector('.focus');
-    var focusWidth = focus.offsetWidth;
-    // 2. 鼠标经过focus 就显示隐藏左右按钮
-    focus.addEventListener('mouseenter', function() {
-        arrow_l.style.display = 'block';
-        arrow_r.style.display = 'block';
-        clearInterval(timer);
-        timer = null; // 清除定时器变量
-    });
-    focus.addEventListener('mouseleave', function() {
-        arrow_l.style.display = 'none';
-        arrow_r.style.display = 'none';
-        timer = setInterval(function() {
-            //手动调用点击事件
-            arrow_r.click();
-        }, 2000);
-    });
-    // 3. 动态生成小圆圈  有几张图片，我就生成几个小圆圈
-    var ul = focus.querySelector('ul');
-    var ol = focus.querySelector('.circle');
-    // console.log(ul.children.length);
-    for (var i = 0; i < ul.children.length; i++) {
-        // 创建一个小li 
-        var li = document.createElement('li');
-        // 记录当前小圆圈的索引号 通过自定义属性来做 
-        li.setAttribute('index', i);
-        // 把小li插入到ol 里面
-        ol.appendChild(li);
-        // 4. 小圆圈的排他思想 我们可以直接在生成小圆圈的同时直接绑定点击事件
-        li.addEventListener('click', function() {
-            // 干掉所有人 把所有的小li 清除 current 类名
-            for (var i = 0; i < ol.children.length; i++) {
-                ol.children[i].className = '';
-            }
-            // 留下我自己  当前的小li 设置current 类名
-            this.className = 'current';
-            // 5. 点击小圆圈，移动图片 当然移动的是 ul 
-            // ul 的移动距离 小圆圈的索引号 乘以 图片的宽度 注意是负值
-            // 当我们点击了某个小li 就拿到当前小li 的索引号
-            var index = this.getAttribute('index');
-            // 当我们点击了某个小li 就要把这个li 的索引号给 num  
-            num = index;
-            // 当我们点击了某个小li 就要把这个li 的索引号给 circle  
-            circle = index;
-            // num = circle = index;
-            console.log(focusWidth);
-            console.log(index);
+// 要点1：移入轮播图，显示左右按钮。移出则隐藏。
+// 要点2：点击按钮，左右翻动轮播图，且小圆圈样式对应改变
+// 要点3：移入小圆圈，轮播图片
+// ❤ 核心算法：ul移动的距离等于小圆圈的索引号乘以图片的宽度
 
-            animate(ul, -index * focusWidth);
+// 要点4：自动播放轮播图片，鼠标移入时，自动播放消失
+// 要点5：通过节流阀实现 前一次轮播动画执行完后才能再次轮播动画
+// 节流阀原理：
+// 在执行之前，先判断flag的值，满足条件则立即修改flag的值，防止本次动画结束前再次被调用。
+// 执行完毕后，在回调函数中修改flag的值，使其可以再次被调用。
+// 其实质相当于同步锁的问题，A一执行则立即将锁拿住，执行结束后再放锁。
+
+window.addEventListener('load', function() {
+    // 要点1：移入轮播图，显示左右按钮。移出则隐藏。
+    var arrow_l = document.querySelector('.arrow-l')
+    var arrow_r = document.querySelector('.arrow-r')
+    var focus = document.querySelector('.focus')
+    focus.addEventListener('mouseenter', function() {
+        arrow_l.style.display = "block"
+        arrow_r.style.display = "block";
+        // 鼠标移入的使用停止自动播放
+        clearInterval(time)
+        time = null
+    })
+    focus.addEventListener('mouseleave', function() {
+        arrow_l.style.display = "none"
+        arrow_r.style.display = "none";
+        // 鼠标移除的时候继续自动播放
+        time = setInterval(function() {
+            arrow_r.click()
+        }, 2000)
+    })
+
+    // 要点2：动态生成小圆圈的个数，使其与图片的个数相同
+    var ul = focus.querySelector('ul')
+    var length = ul.children.length
+    var ol = focus.querySelector('ol')
+    var pictureWidth = focus.offsetWidth
+
+    var num = 0 //当前显示的图片的索引
+    var cicle = 0; //高亮的小圆圈索引
+
+    for (var i = 0; i < length; i++) {
+        var li = document.createElement('li');
+        // 设置自定义属性，方便后续移入小圆圈获取索引值
+        li.setAttribute('data-index', i);
+        // 为小圆圈绑定移入事件
+        li.addEventListener('mouseenter', function() {
+            for (var i = 0; i < ol.children.length; i++) {
+                ol.children[i].className = ""
+            }
+            this.className = "current"
+            var index = this.dataset.index;
+            // 核心算法：ul移动的距离等于小圆圈的索引号乘以图片的宽度
+            animate(ul, -index * pictureWidth);
+            // 当鼠标移入小圆圈后，将索引值赋值给num，方便点击->按钮时正确显示
+            num = index;
+            // 同上，为了让小圆圈的索引值与之对应，应该将index赋值给cicle
+            cicle = index;
+            console.log(num);
         })
+        ol.appendChild(li)
     }
-    // 把ol里面的第一个小li设置类名为 current
-    ol.children[0].className = 'current';
-    // 6. 克隆第一张图片(li)放到ul 最后面
-    var first = ul.children[0].cloneNode(true);
-    ul.appendChild(first);
-    // 7. 点击右侧按钮， 图片滚动一张
-    var num = 0;
-    // circle 控制小圆圈的播放
-    var circle = 0;
-    // flag 节流阀
-    var flag = true;
+    ol.children[0].className = "current"
+
+
+    // 自动创建最后的图片，可以解决小圆圈多了一个的问题
+    var lastLi = ul.children[0].cloneNode(true)
+    ul.appendChild(lastLi)
+
+    // 要点3：点击→按钮，切换下一张
+    var flag = true
     arrow_r.addEventListener('click', function() {
         if (flag) {
-            flag = false; // 关闭节流阀
-            // 如果走到了最后复制的一张图片，此时 我们的ul 要快速复原 left 改为 0
+            flag = false
+                // ❤ 无缝切换的实现：
+                // 在html文件中为图片的后面再添加一张第一张的图片
+                // 当显示出最后添加的图片后，将ul的left设置为0，即恢复成初始状态，便实现了无缝切换
             if (num == ul.children.length - 1) {
                 ul.style.left = 0;
                 num = 0;
             }
-            num++;
-            animate(ul, -num * focusWidth, function() {
-                flag = true; // 打开节流阀
-            });
-            // 8. 点击右侧按钮，小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
-            circle++;
-            // 如果circle == 4 说明走到最后我们克隆的这张图片了 我们就复原
-            if (circle == ol.children.length) {
-                circle = 0;
-            }
-            // 调用函数
-            circleChange();
-        }
-    });
+            num++
+            console.log(num);
+            animate(ul, -num * pictureWidth, function() {
+                // 动画执行完后，将flag设置为true,此时点击->按钮才可以继续切换下一张图片
+                flag = true
+            })
 
-    // 9. 左侧按钮做法
+            // 点击按钮切换时控制小圆圈的样式
+            cicle++
+            if (cicle == ol.children.length) {
+                cicle = 0
+            }
+            cicleChange()
+        }
+    })
+
+
     arrow_l.addEventListener('click', function() {
         if (flag) {
             flag = false;
+            // ❤ 无缝切换的实现：
+            // 在html文件中为图片的后面再添加一张第一张的图片
+            // 当显示出最后添加的图片后，将ul的left设置为0，即恢复成初始状态，便实现了无缝切换
             if (num == 0) {
                 num = ul.children.length - 1;
-                ul.style.left = -num * focusWidth + 'px';
-
+                ul.style.left = -num * pictureWidth + 'px';
             }
-            num--;
-            animate(ul, -num * focusWidth, function() {
-                flag = true;
-            });
-            // 点击左侧按钮，小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
-            circle--;
-            // 如果circle < 0  说明第一张图片，则小圆圈要改为第4个小圆圈（3）
-            // if (circle < 0) {
-            //     circle = ol.children.length - 1;
-            // }
-            circle = circle < 0 ? ol.children.length - 1 : circle;
-            // 调用函数
-            circleChange();
-        }
-    });
+            num--
+            console.log(num);
+            animate(ul, -num * pictureWidth, function() {
+                flag = true
+            })
 
-    function circleChange() {
-        // 先清除其余小圆圈的current类名
+            // 点击按钮切换时控制小圆圈的样式
+            cicle--
+            if (cicle < 0) {
+                cicle = ol.children.length - 1
+            }
+            cicleChange()
+        }
+
+    })
+
+    function cicleChange() {
         for (var i = 0; i < ol.children.length; i++) {
-            ol.children[i].className = '';
+            ol.children[i].className = ""
         }
-        // 留下当前的小圆圈的current类名
-        ol.children[circle].className = 'current';
+        ol.children[cicle].className = "current"
     }
-    // 10. 自动播放轮播图
-    var timer = setInterval(function() {
-        //手动调用点击事件
-        arrow_r.click();
-    }, 2000);
 
+    // 实现自动轮播播放图片的原理:设置定时器,手动调用->点击事件
+    var time = setInterval(function() {
+        arrow_r.click()
+    }, 2000)
 })
